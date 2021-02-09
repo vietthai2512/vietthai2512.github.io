448bceb8abbbcfe83620d632889ab1bd1ee48e07
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-02-10 01:24:53 +0700

    🔨 use a better code

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-console.log('dame');
+const nav_children = document.getElementById('header').querySelectorAll('a');
 
-
-/*for (let nav_a of nav)
+nav_children.forEach((elem) =>
 {
-    nav_a.addEventListener("'click'", () => 
+    elem.addEventListener('click', () => 
     {
-        console.log('clicked');
-    }, false);
-}*/
-addEventListener('click', function (event)
-{
-    const anchor = event.target.closest("a");   // Find closest Anchor (or self)
-    if (!anchor) return;
-
-    const home_a_tag = document.getElementById('home_a_tag');
-    home_a_tag.removeAttribute('style');
-
-    const nav_children = document.getElementById('nav').children;
-    for (let nav_child of nav_children)
-    {
-        nav_child.removeAttribute('style');
-    }
-
-    // Not found. Exit here.
-    anchor.setAttribute('style', 'border-bottom: 1px solid');
-    console.log(anchor.getAttribute('href'));  // Log to test
+        for (let nav_child of nav_children)
+            nav_child.removeAttribute('style');
+        elem.setAttribute('style', 'border-bottom: 1px solid');
+    });
 });
