3b659b8efbe417f696e6de87225858d8dbdbbb2a
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-02-10 23:10:49 +0700

    :bug: fix responsive issue on less - more

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ nav_children.forEach((elem) =>
         elem.setAttribute('style', 'border-bottom: 1px solid');
     });
 });
+
+const svg = document.querySelector('div.float-right.text-gray').querySelectorAll('svg');
+svg.forEach((elem) =>
+{
+
+    let width = elem.getAttribute("width");
+    let height = elem.getAttribute("height");
+    // Remove height property entirely
+    elem.removeAttribute("height");
+    // Width property should be set to 100% to fill entire container
+    elem.setAttribute("width", "1.3%");
+    // Add a viewBox property based on the former width/height
+    elem.setAttribute("viewBox", "0 0 " + width + " " + height);
+});
